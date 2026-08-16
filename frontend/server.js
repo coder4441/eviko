b67471d28a2b7b992d5c84b7d70798ddf1a1e79b
EVIKO <eviko@example.com>
2026-08-16 19:40:05 +0500

    Add server.js for Phusion Passenger

--- frontend/server.js
+++ frontend/server.js
@@ -1,31 +1,21 @@
+#!/usr/bin/env node
+// Phusion Passenger uchun Next.js server
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      await handle(req, res, parsedUrl);
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('internal server error');
-    }
-  })
-    .once('error', (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        handle(req, res, parsedUrl);
+    }).listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on port ${port}`);
     });
 });
